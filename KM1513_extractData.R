1730a52b48fe71bebca050bccfd4d6d35e4e7f8b
Jamie Collins <james.r.collins@aya.yale.edu>
2018-03-26 18:48:38 -0700

    Initial commit KM1513_extractData.R

---
+++ KM1513_extractData.R
@@ -0,0 +1,7 @@
+# KM1513_extractData.R
+
+# Purpose: Extract desired CTD and underway/met ship data corresponding to exact
+# CTD bottle closure times during cruise KM1513 aboard R/V Kilo Moana,
+# 24 Jul - 6 Aug 2015; this was the SCOPE "diel" experiment (HOE-Legacy 2)
+
+# Created 3/26/2018 by J.R.C.
